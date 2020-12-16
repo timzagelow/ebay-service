84140388071f9310ea917fb956f76b3df8d300ee
timzagelow <timzagelow@gmail.com>
2020-12-16 15:24:50 -0800

    fix miscalculation

--- builders/title.js
+++ builders/title.js
@@ -5,15 +5,15 @@ function build(item) {
 
     let title = `${itemBuilder.artist()}: ${itemBuilder.title()}`;
 
-    if ((title.length + item.release.label.name.length) <= process.env.EBAY_TITLE_MAX_LENGTH) {
+    if ((title.length + item.release.label.name.length + ` `) <= process.env.EBAY_TITLE_MAX_LENGTH) {
         title += ` ${item.release.label.name}`;
     }
 
-    if ((title.length + itemBuilder.size().length) <= process.env.EBAY_TITLE_MAX_LENGTH) {
+    if ((title.length + itemBuilder.size().length + ` `) <= process.env.EBAY_TITLE_MAX_LENGTH) {
         title += ` ${itemBuilder.size()}`;
     }
 
-    if ((title.length + itemBuilder.duration().length) <= process.env.EBAY_TITLE_MAX_LENGTH) {
+    if ((title.length + itemBuilder.duration().length + ` `) <= process.env.EBAY_TITLE_MAX_LENGTH) {
         title += ` ${itemBuilder.duration()}`;
     }
 
