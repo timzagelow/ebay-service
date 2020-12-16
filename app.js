6a23c47d0909272ee1e3039bb684fb05717f8e07
timzagelow <timzagelow@gmail.com>
2020-12-16 10:48:04 -0800

    use commented-out app.js for now

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const queue = require('./queue');
 (async() => {
     try {
         // await queue.buildCacheQueue.add({}, {repeat: {cron: '*/15 * * * *'}});
-        await queue.buildCacheQueue.pause();
+        // await queue.buildCacheQueue.pause();
 
-        await queue.itemQueue.add({ jobs: [
-                { type: 'remove', itemId: 577991, listingId: "5fd81578be3a8e0d7fc24543" },
+        // await queue.itemQueue.add({ jobs: [
+        //         { type: 'add', itemId: 680290, listingId: "5fd81578be3a8e0d7fc2454f" },
         //         // { type: 'remove', itemId: 577991 },
         //         // { type: 'remove', itemId: 717570 },
         //         // { type: 'remove', itemId: 152465 },
-            ]});
+        //     ]});
     } catch (error) {
         // console.log(error);
     }
