2471b8785a20684a6062352cc72b1b307a070c32
timzagelow <timzagelow@gmail.com>
2020-12-21 12:08:48 -0800

    change to POST for fulfillment endpoint

--- api/partner/order.js
+++ api/partner/order.js
@@ -20,7 +20,7 @@ async function refund(orderId, amount, reason) {
 }
 
 async function ship(orderId, payload) {
-    const { data } = await axios.get(`${process.env.EBAY_API_URL}/sell/fulfillment/v1/order/${orderId}/shipping_fulfillment`, payload);
+    const { data } = await axios.post(`${process.env.EBAY_API_URL}/sell/fulfillment/v1/order/${orderId}/shipping_fulfillment`, payload);
 
     return data;
 }
