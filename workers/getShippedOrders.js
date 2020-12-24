ef552813f20bf2e09fe621326676f2f29cad981b
timzagelow <timzagelow@gmail.com>
2020-12-24 10:30:34 -0800

    setting the correct key for lastShippedFetch

--- workers/getShippedOrders.js
+++ workers/getShippedOrders.js
@@ -25,7 +25,7 @@ async function getShippedOrders() {
 
 async function setLastFetch() {
     let date = moment().toISOString();
-    await setAsync('lastOrdersFetch', date);
+    await setAsync('lastShippedFetch', date);
 }
 
 module.exports = getShippedOrders;
