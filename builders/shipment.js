020fb1bd77d74205c4170367494af19704992e34
timzagelow <timzagelow@gmail.com>
2020-12-28 12:43:56 -0800

    use landmark carrier code when applicable

--- builders/shipment.js
+++ builders/shipment.js
@@ -3,11 +3,13 @@ const internalOrder = require('../api/internal/order');
 async function buildShipment(orderId) {
     const order = await internalOrder.fetch(orderId);
 
+    let carrierCode = order.shipping.shipments[0].tracking.includes('LTN') ? process.env.LANDMARK_SHIPPING_CARRIER_CODE : process.env.USPS_SHIPPING_CARRIER_CODE;
+
     return {
         lineItems: buildLineItems(order.items),
         shippedDate: order.shipping.shipments[0].date,
         trackingNumber: order.shipping.shipments[0].tracking,
-        shippingCarrierCode: process.env.EBAY_SHIPPING_CARRIER_CODE,
+        shippingCarrierCode: carrierCode,
     };
 }
 
