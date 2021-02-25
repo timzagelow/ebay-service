fc6ba979bafdad69606e4ea1486ca2b0eb2b4a6b
timzagelow <timzagelow@gmail.com>
2021-02-25 08:55:38 -0800

    change query for getting orders

--- api/partner/order.js
+++ api/partner/order.js
@@ -4,7 +4,7 @@ const moment = require('moment');
 async function fetchNew(lastCheck) {
     let date = moment(lastCheck).toISOString();
 
-    const { data } = await axios.get(`${process.env.EBAY_API_URL}/sell/fulfillment/v1/order?limit=500&filter=creationdate:[${date}]`);
+    const { data } = await axios.get(`${process.env.EBAY_API_URL}/sell/fulfillment/v1/order?limit=500&filter=creationdate:%5B${date}..%5D`);
 
     if (data.orders) {
         return data.orders;
