90be994fec36d18954bb9f31830f89e31d3c6f40
timzagelow <timzagelow@gmail.com>
2021-06-14 08:40:19 -0700

    use defaults for worker settings

--- queue/workers.js
+++ queue/workers.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const defaults = require('./defaults');
 const itemProcessor = require('./itemProcessor');
 const orderProcessor = require('./orderProcessor');
 
@@ -10,11 +11,11 @@ const createOrderHandler = require('../handlers/createOrder');
 const shipOrderHandler = require('../handlers/shipOrder');
 
 const RSMQWorker = require('rsmq-worker');
-const addItemWorker = new RSMQWorker(process.env.ADD_ITEM_QUEUE, { autostart: true });
-const removeItemWorker = new RSMQWorker(process.env.REMOVE_ITEM_QUEUE, { autostart: true });
-const updateItemWorker = new RSMQWorker(process.env.UPDATE_ITEM_QUEUE, { autostart: true });
-const createOrderWorker = new RSMQWorker(process.env.CREATE_ORDER_QUEUE, { autostart: true });
-const shipOrderWorker = new RSMQWorker(process.env.SHIP_ORDER_QUEUE, { autostart: true });
+const addItemWorker = new RSMQWorker(process.env.ADD_ITEM_QUEUE, defaults.workerSettings);
+const removeItemWorker = new RSMQWorker(process.env.REMOVE_ITEM_QUEUE, defaults.workerSettings);
+const updateItemWorker = new RSMQWorker(process.env.UPDATE_ITEM_QUEUE, defaults.workerSettings);
+const createOrderWorker = new RSMQWorker(process.env.CREATE_ORDER_QUEUE, defaults.workerSettings);
+const shipOrderWorker = new RSMQWorker(process.env.SHIP_ORDER_QUEUE, defaults.workerSettings);
 
 async function init() {
     itemProcessor.init(addItemWorker, addItemHandler);
